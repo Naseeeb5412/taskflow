738530df165754a3c6a4561667746ad2ab378f1e
Naseeb Latheef <naseeblatheef4@gmail.com>
2026-04-20 17:18:01 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,11 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
-    ? `${import.meta.env.VITE_API_URL}/api`
-    : '/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true,
-})
+});
 
 api.interceptors.response.use(
   (res) => res,
